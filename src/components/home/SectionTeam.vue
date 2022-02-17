f2bd2fb6334960b4cd6b752032f3869d19e6c46d
cotton123236 <cotton123236@gmail.com>
2022-02-17 21:30:18 +0800

    change img

--- src/components/home/SectionTeam.vue
+++ src/components/home/SectionTeam.vue
@@ -37,7 +37,7 @@ const teamBlockData = reactive([
   },
   {
     id: '04',
-    name: 'Robert D.',
+    name: 'Williams',
     title: 'Chief Marketing Officer',
     list: [
       '3+ years of experience in project management',
